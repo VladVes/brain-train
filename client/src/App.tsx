da6fbd577e9196d3f5dee4cb3d0c4ed2b5ff8528
VladVes <vccs@inbox.ru>
2026-08-19 14:51:12 +0700

    init client with react and vite, add root page

---
+++ client/src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="terminal">
+      <h1 className="title">Brain-Train</h1>
+
+      <main className="content">
+        <p className="welcome">
+          Welcome to the Brain-train! This App will helps you to improve your
+          working memory be mentally converting numbers between numeral systems
+        </p>
+      </main>
+
+      <div className="scanlines" aria-hidden="true"></div>
+    </div>
+  )
+}
+
+export default App
